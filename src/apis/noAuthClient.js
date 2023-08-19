668dbf2a19e9fc57c6367d6ebc56208726d85407
Sun Yerim <syl1602@naver.com>
2023-08-19 15:23:12 +0900

    통신작업 확인

---
+++ src/apis/noAuthClient.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const noAuthClient = axios.create({
+  baseURL: 'http://15.164.50.203:3000',
+
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default noAuthClient;
