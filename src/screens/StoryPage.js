33527b074149d862601abc41f1896494f5c4fce4
Chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2023-08-27 23:39:35 +0900

    책 두 권 만들기

--- src/screens/StoryPage.js
+++ src/screens/StoryPage.js
@@ -29,29 +29,12 @@ export default CounsellingRe = () => {
   const [loading, setLoading] = useState(false);
   const [speaking, setSpeaking] = useState(false);
   const [count, setCount] = useState(0);
-  const [gptLastLetter, setGptLastLetter] = useState('');
-  const scrollViewRef = useRef();
+  const [book, setBook] = useState(0);
 
   const fetchResponse = () => {
     if (result.trim().length > 0) {
       let newMessages = [...messages];
     }
-  };
-  const startTextToSpeech = message => {
-    if (!message.content.includes('https')) {
-      Tts.getInitStatus().then(() => {
-        Tts.speak(message.content, {
-          iosVoiceId: 'com.apple.ttsbundle.Yuna-compact',
-          rate: 0.6,
-        });
-      });
-    }
-  };
-
-  const updateScrollView = () => {
-    setTimeout(() => {
-      scrollViewRef?.current?.scrollToEnd({animated: true});
-    }, 200);
   };
 
   const clear = () => {
@@ -130,26 +113,50 @@ export default CounsellingRe = () => {
   }, []);
 
   const storyMessage = [
-    '옛날에 수연이라는 꼬마 펭귄이 살았어요',
-    '수연이의 배에서 꼬르륵 소리가 났어요',
-    '아이 배고파 먹을 거 없나',
-    '수연이는 냉장고를 열어봤지만 텅 비어있었어요',
-    '귀찮아서 물고기를 잡아오지 않았기 때문이에요',
-    '심심해진 수연이는 좋아하는 펭귄 친구 준성이에게 전화했어요',
-    '나 심심한데 오늘 우리집에서 놀래',
-    '으잉 너네집에서',
-    '응 같이 물고기 먹자',
-    '좋아',
-    '수연이는 열심히 집을 치우고 꽃단장을 했어요',
-    '똑똑똑 수연아 나야',
-    '어서와 우리 뭐하고 놀까',
-    '같이 물고기 먹자며',
-    '수연이는 텅 빈 냉장고가 떠올랐어요',
-    '어쩌지 물고기가 없는 것을 깜빡했어',
-    '그럼 다음에 올게 안녕',
-    '수연이는 슬퍼서 엉엉 울었어요',
-    '진작 물고기 잡아놓을 걸',
-    '그 날부터 수연이는 미루지 않고 꼬박꼬박 물고기를 잡으러 갔답니다',
+    [
+      '옛날에 수연이라는 꼬마 펭귄이 살았어요',
+      '수연이의 배에서 꼬르륵 소리가 났어요',
+      '아이 배고파 먹을 거 없나',
+      '수연이는 냉장고를 열어봤지만 텅 비어있었어요',
+      '귀찮아서 물고기를 잡아오지 않았기 때문이에요',
+      '심심해진 수연이는 좋아하는 펭귄 친구 준성이에게 전화했어요',
+      '나 심심한데 오늘 우리집에서 놀래',
+      '으잉 너네집에서',
+      '응 같이 물고기 먹자',
+      '좋아',
+      '수연이는 열심히 집을 치우고 꽃단장을 했어요',
+      '똑똑똑 수연아 나야',
+      '어서와 우리 뭐하고 놀까',
+      '같이 물고기 먹자며',
+      '수연이는 텅 빈 냉장고가 떠올랐어요',
+      '어쩌지 물고기가 없는 것을 깜빡했어',
+      '그럼 다음에 올게 안녕',
+      '수연이는 슬퍼서 엉엉 울었어요',
+      '진작 물고기 잡아놓을 걸',
+      '그 날부터 수연이는 미루지 않고 꼬박꼬박 물고기를 잡으러 갔답니다',
+    ],
+    [
+      '옛날에 선우라는 꼬마 펭귄이 살았어요',
+      '수연이의 배에서 꼬르륵 소리가 났어요',
+      '아이 배고파 먹을 거 없나',
+      '수연이는 냉장고를 열어봤지만 텅 비어있었어요',
+      '귀찮아서 물고기를 잡아오지 않았기 때문이에요',
+      '심심해진 수연이는 좋아하는 펭귄 친구 준성이에게 전화했어요',
+      '나 심심한데 오늘 우리집에서 놀래',
+      '으잉 너네집에서',
+      '응 같이 물고기 먹자',
+      '좋아',
+      '수연이는 열심히 집을 치우고 꽃단장을 했어요',
+      '똑똑똑 수연아 나야',
+      '어서와 우리 뭐하고 놀까',
+      '같이 물고기 먹자며',
+      '수연이는 텅 빈 냉장고가 떠올랐어요',
+      '어쩌지 물고기가 없는 것을 깜빡했어',
+      '그럼 다음에 올게 안녕',
+      '수연이는 슬퍼서 엉엉 울었어요',
+      '진작 물고기 잡아놓을 걸',
+      '그 날부터 수연이는 미루지 않고 꼬박꼬박 물고기를 잡으러 갔답니다',
+    ],
   ];
 
   return (
@@ -161,6 +168,20 @@ export default CounsellingRe = () => {
             source={require('../../assets/images/bot.png')}
             style={{height: hp(15), width: hp(15)}}
           />
+        </View>
+        {/* 책 선택창 */}
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <TouchableOpacity onPress={() => setBook(0)}>
+            <Text>게으른 꼬마 펭귄</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setBook(1)}>
+            <Text>부지런한 꼬마 펭귄</Text>
+          </TouchableOpacity>
         </View>
         {/* feature,message */}
         <View style={{flex: 1.5, marginVertical: 10}}>
@@ -175,35 +196,44 @@ export default CounsellingRe = () => {
               <Text style={styles.assistantHeading}>
                 이야기를 따라해봐~!!!!
               </Text>
-              <Text>{storyMessage[count]}</Text>
+              <Text>{storyMessage[book][count]}</Text>
               <Text>{result}</Text>
-              {count > 0 && (
-                <TouchableOpacity
-                  onPress={() => {
-                    setCount(count - 1);
-                  }}>
-                  <Text>이전</Text>
-                </TouchableOpacity>
-              )}
-              {count < storyMessage.length - 1 && (
-                <TouchableOpacity
-                  onPress={() => {
-                    if (
-                      storyMessage[count].replace(/(\s*)/g, '') ===
-                      result.replace(/(\s*)/g, '')
-                    ) {
-                      setCount(count + 1);
-                      Alert.alert('잘했어요!');
-                      setResult('');
-                    } else {
-                      console.log(storyMessage[count].replace(/(\s*)/g, ''));
-                      console.log(result.replace(/(\s*)/g, ''));
-                      Alert.alert('조금 더 정확하게 말해볼까요?');
-                    }
-                  }}>
-                  <Text>다음</Text>
-                </TouchableOpacity>
-              )}
+              <View
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                }}>
+                {count > 0 && (
+                  <TouchableOpacity
+                    style={{marginRight: 8}}
+                    onPress={() => {
+                      setCount(count - 1);
+                    }}>
+                    <Text>이전</Text>
+                  </TouchableOpacity>
+                )}
+                {count < storyMessage[book].length - 1 && (
+                  <TouchableOpacity
+                    onPress={() => {
+                      if (
+                        storyMessage[book][count].replace(/(\s*)/g, '') ===
+                        result.replace(/(\s*)/g, '')
+                      ) {
+                        setCount(count + 1);
+                        Alert.alert('잘했어요!');
+                        setResult('');
+                      } else {
+                        console.log(
+                          storyMessage[book][count].replace(/(\s*)/g, ''),
+                        );
+                        console.log(result.replace(/(\s*)/g, ''));
+                        Alert.alert('조금 더 정확하게 말해볼까요?');
+                      }
+                    }}>
+                    <Text>다음</Text>
+                  </TouchableOpacity>
+                )}
+              </View>
             </View>
           </View>
         </View>
