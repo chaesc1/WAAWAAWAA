2b95ddac5a022fb32b99558f2377f2d5097a9b2e
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-09-25 02:32:06 +0900

    유효성 검사

--- src/screens/newRegister.js
+++ src/screens/newRegister.js
@@ -10,6 +10,7 @@ import {
   Keyboard,
   TouchableOpacity,
   Image,
+  ScrollView,
 } from 'react-native';
 import {themeColors} from '../../theme';
 import {SafeAreaView} from 'react-native-safe-area-context';
@@ -30,6 +31,13 @@ export default function SignUpScreen() {
   const [password, setPassword] = useState('');
   const [age, setAge] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [PasswordLength, setPasswordLength] = useState('');
+  const [ConfirmPasswordLength, setConfirmPasswordLength] = useState('');
+  //오류메시지 상태저장
+  const [nameMessage, setNameMessage] = useState('');
+  const [idMessage, setIdMessage] = useState('');
+  const [passwordMessage, setPasswordMessage] = useState('');
+  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState('');
 
   // 유효성 검사
   const [isName, setIsName] = useState(false);
@@ -40,42 +48,59 @@ export default function SignUpScreen() {
 
   // 이름
   const onChangeNickName = useCallback(e => {
-    setUsername(e.nativeEvent.text);
+    const nickname = e.nativeEvent.text;
+    setUsername(nickname);
+    if (nickname.length < 3 || nickname.length > 15) {
+      setNameMessage('3글자 이상 15글자 미만으로 입력해주세요.');
+      setIsName(false);
+    } else {
+      setNameMessage('올바른 닉네임 형식입니다 :)');
+      setIsName(true);
+    }
   }, []);
 
   // 아이디
   const onChangeId = useCallback(e => {
     const idCurrent = e.nativeEvent.text;
     setUserId(idCurrent);
-
-    setIsId(true);
-  }, []);
-
-  // 비밀번호
-  const onChangePassword = useCallback(e => {
-    const passwordRegex =
-      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
-    const passwordCurrent = e.nativeEvent.text;
-    setPassword(passwordCurrent);
-
-    if (!passwordRegex.test(passwordCurrent)) {
-      setIsPassword(false);
+    if (idCurrent.length < 3 || idCurrent.length > 15) {
+      setIdMessage('3글자 이상 15글자 미만으로 입력해주세요.');
+      setIsId(false);
     } else {
-      setIsPassword(true);
+      setIdMessage('올바른 ID 형식입니다.');
+      setIsId(true);
     }
   }, []);
-
+  // 비밀번호
+  const onChangePassword = useCallback(e => {
+    // const passwordRegex =
+    //   /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+    // const passwordCurrent = e.target.value;
+    // console.log(passwordRegex.test(passwordCurrent));
+    // setPassword(passwordCurrent);
+    // if (!passwordRegex.test(passwordCurrent)) {
+    //   console.log(`입력된 비밀번호${passwordCurrent}`);
+    //   setPasswordMessage(
+    //     '숫자,영문자,특수문자 조합으로 8자리 이상 입력해주세요!',
+    //   );
+    //   setIsPassword(false);
+    // } else {
+    //   setPasswordMessage('안전한 비밀번호에요 : )');
+    //   setIsPassword(true);
+    // }
+  }, []);
   // 비밀번호 확인
   const onChangePasswordConfirm = useCallback(
     e => {
-      const passwordConfirmCurrent = e.nativeEvent.text;
-      setConfirmPassword(passwordConfirmCurrent);
-
-      if (password === passwordConfirmCurrent) {
-        setIsPasswordConfirm(true);
-      } else {
-        setIsPasswordConfirm(false);
-      }
+      // const passwordConfirmCurrent = e.target.value;
+      // setConfirmPassword(passwordConfirmCurrent);
+      // if (password == passwordConfirmCurrent) {
+      //   setIsPasswordConfirm(false);
+      //   setPasswordConfirmMessage('비밀번호를 똑같이 입력했어요 : )');
+      // } else {
+      //   setIsPasswordConfirm(false);
+      //   setPasswordConfirmMessage('비밀번호가 틀려요. 다시 확인해주세요');
+      // }
     },
     [password],
   );
@@ -92,30 +117,37 @@ export default function SignUpScreen() {
   }, []);
   // 유저생성 요청 api
   const register = async () => {
-    console.log(username, userId, password, age);
-    try {
-      const res = await noAuthClient({
-        method: 'post',
-        url: `/users`,
-        data: {
-          username: username,
-          userId: userId,
-          password: password,
-          age: Number(age),
-        },
-      });
+    // 모든 필드의 유효성을 확인
+    if (isName && isId && isPassword && isPasswordConfirm && isAge) {
+      console.log(username, userId, password, age);
+      try {
+        const res = await noAuthClient({
+          method: 'post',
+          url: `/users`,
+          data: {
+            username: username,
+            userId: userId,
+            password: password,
+            age: Number(age),
+          },
+        });
 
-      if (res.status === 201) {
-        Alert.alert('유저 생성 성공');
-        navigation.navigate('LoginPage'); // 회원가입 성공 시 로그인 페이지로 이동
-      } else {
-        // 서버 응답이 실패인 경우
-        Alert.alert('유저 생성 실패', '유저 생성에 실패했습니다.');
+        if (res.status === 201) {
+          Alert.alert('유저 생성 성공');
+          navigation.navigate('LoginPage'); // 회원가입 성공 시 로그인 페이지로 이동
+        } else {
+          // 서버 응답이 실패인 경우
+          Alert.alert('유저 생성 실패', '유저 생성에 실패했습니다.');
+        }
+      } catch (error) {
+        console.log('Test Error:', error.response);
       }
-    } catch (error) {
-      console.log('Test Error:', error.response);
+    } else {
+      // 필드 중 하나라도 유효하지 않으면 오류 메시지 표시
+      Alert.alert('입력 정보를 확인하세요', '유효하지 않은 정보가 있습니다.');
     }
   };
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
@@ -127,6 +159,7 @@ export default function SignUpScreen() {
               <ArrowLeftIcon size={wp('6%')} color="white" />
             </TouchableOpacity>
           </View>
+          {/* 이미지 */}
           <View style={styles.imageContainer}>
             <Lottie
               source={require('../../assets/animations/Register.json')}
@@ -138,7 +171,7 @@ export default function SignUpScreen() {
           </View>
         </SafeAreaView>
         <View style={styles.formContainer}>
-          <View style={styles.form}>
+          <ScrollView style={styles.form}>
             <Text style={styles.label}>Name</Text>
             <TextInput
               style={styles.input}
@@ -150,6 +183,13 @@ export default function SignUpScreen() {
               underlineColorAndroid="#f000"
               blurOnSubmit={false}
             />
+            {username.length > 0 && (
+              <Text
+                style={{color: 'red'}}
+                className={`message ${isName ? 'success' : 'error'}`}>
+                {nameMessage}{' '}
+              </Text>
+            )}
             <Text style={styles.label}>ID</Text>
             <TextInput
               style={styles.input}
@@ -161,6 +201,11 @@ export default function SignUpScreen() {
               underlineColorAndroid="#f000"
               blurOnSubmit={false}
             />
+            {userId.length > 0 && (
+              <Text className={`message ${isId ? 'success' : 'error'}`}>
+                {idMessage}{' '}
+              </Text>
+            )}
             <Text style={styles.label}>Password</Text>
             <TextInput
               style={styles.input}
@@ -169,11 +214,38 @@ export default function SignUpScreen() {
               autoCapitalize="none"
               returnKeyType="next"
               value={password}
-              onChange={onChangePassword}
+              onChangeText={text => {
+                console.log(`비밀번호: ${text}`);
+                const Length = text.length;
+                setPasswordLength(Length);
+                console.log(`비밀번호 길이: ${PasswordLength}`);
+
+                const passwordRegex =
+                  /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+                const passwordCurrent = text;
+                // console.log(passwordRegex.test(passwordCurrent));
+                setPassword(passwordCurrent);
+                if (!passwordRegex.test(passwordCurrent)) {
+                  // console.log(`입력된 비밀번호${passwordCurrent}`);
+                  setPasswordMessage(
+                    '숫자,영문자,특수문자 조합으로 8자리 이상 입력해주세요!',
+                  );
+                  setIsPassword(false);
+                } else {
+                  setPasswordMessage('안전한 비밀번호에요 : )');
+                  setIsPassword(true);
+                }
+              }}
+              // onChange={onChangePassword}
               underlineColorAndroid="#f000"
               blurOnSubmit={false}
             />
-            <Text style={styles.label}>Password</Text>
+            {PasswordLength > 0 && (
+              <Text className={`message ${isPassword ? 'success' : 'error'}`}>
+                {passwordMessage}{' '}
+              </Text>
+            )}
+            <Text style={styles.label}>ReEnter Password</Text>
             <TextInput
               style={styles.input}
               secureTextEntry
@@ -181,10 +253,35 @@ export default function SignUpScreen() {
               autoCapitalize="none"
               returnKeyType="next"
               value={confirmPassword}
-              onChange={onChangePasswordConfirm}
+              onChangeText={text => {
+                console.log(`비밀번호: ${text} ${password}`);
+                const Length = text.length;
+                setConfirmPasswordLength(Length);
+                console.log(`비밀번호 길이: ${ConfirmPasswordLength}`);
+                const passwordConfirmCurrent = text;
+                setConfirmPassword(passwordConfirmCurrent);
+                if (password == passwordConfirmCurrent) {
+                  setIsPasswordConfirm(false);
+                  setPasswordConfirmMessage('비밀번호를 똑같이 입력했어요 : )');
+                } else {
+                  setIsPasswordConfirm(false);
+                  setPasswordConfirmMessage(
+                    '비밀번호가 틀려요. 다시 확인해주세요',
+                  );
+                }
+              }}
+              // onChange={onChangePasswordConfirm}
               underlineColorAndroid="#f000"
               blurOnSubmit={false}
             />
+            {ConfirmPasswordLength > 0 && (
+              <Text
+                className={`message ${
+                  isPasswordConfirm ? 'success' : 'error'
+                }`}>
+                {passwordConfirmMessage}{' '}
+              </Text>
+            )}
             <Text style={styles.label}>Age</Text>
             <TextInput
               style={styles.input}
@@ -192,11 +289,11 @@ export default function SignUpScreen() {
               value={age}
               onChange={onChangeAge}
             />
-            {/* 버튼 */}
-            <TouchableOpacity style={styles.signUpButton} onPress={register}>
-              <Text style={styles.signUpButtonText}>회원가입</Text>
-            </TouchableOpacity>
-          </View>
+          </ScrollView>
+          {/* 버튼 */}
+          <TouchableOpacity style={styles.signUpButton} onPress={register}>
+            <Text style={styles.signUpButtonText}>회원가입</Text>
+          </TouchableOpacity>
           <Text style={styles.orText}>Or</Text>
           <View style={styles.loginContainer}>
             <Text style={styles.loginText}>이미 계정이 있나요?</Text>
@@ -283,11 +380,12 @@ const styles = StyleSheet.create({
   loginContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
-    marginTop: hp('2%'),
+    marginTop: hp('3%'),
   },
   loginText: {
     color: 'gray',
     fontWeight: 'bold',
+    marginBottom: hp('4%'),
   },
   loginLink: {
     color: '#004225',
