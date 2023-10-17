27bad8d891ec23a4943acc6cf4bcf7624622521b
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2023-10-17 09:53:30 +0900

    Design/위험의심 내용 수정

--- src/screens/StaticsPage.js
+++ src/screens/StaticsPage.js
@@ -321,6 +321,7 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
+    alignSelf: 'center',
   },
   dangerKeywordContentTitle: {
     fontWeight: 'bold',
